b48de727db4daab255f248fde8a1514956d124bb
Xmuluneh <mulabr673@gmail.com>
2023-01-03 21:43:16 +0300

    Task Completed

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const map1 = new Map();
+  map1.set('Apples', 10);
+  map1.set('Tomatoes', 10);
+  map1.set('Pasta', 1);
+  map1.set('Rice', 1);
+  map1.set('Banana', 5);
+
+  return map1;
+}
