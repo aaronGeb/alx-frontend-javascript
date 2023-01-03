1bba72bec2263e5e5a62d4a95e4ab821aac84b9b
Xmuluneh <mulabr673@gmail.com>
2023-01-03 16:34:33 +0300

    Task one

---
+++ 0x03-ES6_data_manipulation/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+};
---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default function getListStudents() {
+  const objArray = [];
+  objArray.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  objArray.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  return objArray;
+}
