d5cbc0c483742e1fb69f932ef0f5fd90aa9044d0
Xmuluneh <mulabr673@gmail.com>
2023-01-03 21:14:29 +0300

    Task Zero

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,8 @@
 export default function getListStudents() {
-    const objArray = [];
-    objArray.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
-    objArray.push({ id: 2, firstName: 'James', location: 'Columbia' });
-    objArray.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
-  
-    return objArray;
-  }
-  
+  const objArray = [];
+  objArray.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  objArray.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  objArray.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+
+  return objArray;
+}
