564afd9653df64cc4221769798b3ea22c4580e20
Xmuluneh <mulabr673@gmail.com>
2023-01-03 21:27:34 +0300

    Task one

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(objArray) {
   if (Array.isArray(objArray)) {
-      const newArray = objArray.map((student) => student.id);
-      return newArray;
+    const newArray = objArray.map((student) => student.id);
+    return newArray;
   }
   return [];
 }
