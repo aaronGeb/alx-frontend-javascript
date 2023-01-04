4aec690cc6666547bebeb03db23b8ffb2acd16a8
Xmuluneh <mulabr673@gmail.com>
2023-01-04 21:39:52 +0300

    Task Completed

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,17 @@
+interface MajorCredits {
+    type: 'major';
+    credits: number;
+}
+
+interface MinorCredits {
+    type: 'minor';
+    credits: number;
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): number{
+    return subject1.credits + subject2.credits
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number{
+    return subject1.credits + subject2.credits
+}
