239cac790b4204ebf1a816ceeabc3c10e3d6a110
Xmuluneh <mulabr673@gmail.com>
2023-01-04 15:41:47 +0300

    Task zero

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -4,3 +4,52 @@ interface Student{
     age:number;
     location:string
 }
+const student1: Student = {
+    firstName: 'Aaron',
+    lastName: 'Abrham',
+    age: 45,
+    location: 'Ukraine'
+}
+
+const student2: Student = {
+    firstName: 'MIke',
+    lastName: 'Root',
+    age: 45,
+    location: 'London'
+}
+
+const studentArray:Student[] = [student1, student2];
+
+
+let table = document.createElement('table');
+const body = document.body
+
+table.innerHTML = `<tr>
+                      <th>First Name</th>
+                      <th>Location</th>
+                   </tr>`
+
+studentArray.forEach((student: Student): void => {
+    const row = `<tr>
+                  <td>${student.firstName}</td>
+                  <td>${student.location}</td>
+                 </tr>`
+    table.innerHTML += row;
+})
+
+body.append(table)
+
+table.style.border = "3px solid purple";
+const th = document.getElementsByTagName('th');
+
+for (let i = 0; i < th.length; i++)
+{
+    th[i].style.border = "2px solid black";
+}
+
+const td = document.getElementsByTagName('td');
+
+for (let i = 0; i < td.length; i++)
+{
+    td[i].style.border = "1px solid black";
+}
