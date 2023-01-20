dfa3ea4dd8cf60100d4d3fd22865ec81533b07a1
Xmuluneh <mulabr673@gmail.com>
2023-01-20 20:30:18 +0300

    Task Completed

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const obj = [];
+  obj.push(new ClassRoom(19));
+  obj.push(new ClassRoom(20));
+  obj.push(new ClassRoom(34));
+}
