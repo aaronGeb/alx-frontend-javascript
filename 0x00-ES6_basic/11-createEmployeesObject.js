f4e77a0149aecf36fa30f9c74f29d4b6a869e677
Xmuluneh <mulabr673@gmail.com>
2023-01-24 16:50:19 +0300

    task completd

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   const dep = {
-    '{$departmentName}': employees,
+    '{$departmentName}': [employees],
 
   };
   return dep;
